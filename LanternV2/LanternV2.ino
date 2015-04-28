83a960ec9d7d87b33a15c79246e68b346307717f
TESSIER Alexis <atessier@p-212b-03.intra.gobelins.fr>
2015-04-28 20:25:08 +0200

    Test bluetooth/leds et nano

--- LanternV2/LanternV2.ino
+++ LanternV2/LanternV2.ino
@@ -28,11 +28,31 @@
 //#define SD_CARD_PIN 10
 //TMRpcm audio;
 
+/*===============================*/
+/*=====Gestion des neopixels=====*/
+/*===============================*/
+
+#define LEDS_PIN 9
+#define NUMBER_OF_LEDS 2
+
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUMBER_OF_LEDS, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void rgb(byte index, byte red, byte green, byte blue){
+  leds.setPixelColor(index, green, red, blue);
+}
+
+void rgb(byte red, byte green, byte blue){
+  byte i;
+  for(i=0;i<NUMBER_OF_LEDS;i++){
+    rgb(i, red, green, blue);
+  }  
+}
 /*==============================*/
 /*=====Gestion du bluetooth=====*/
 /*==============================*/
 
-#define REQ_PIN 8
+#define REQ_PIN 10
 #define RDY_PIN 2
 #define RST_PIN 7
 
@@ -52,13 +72,23 @@ void sendMessage(char *messageToSend, uint8_t lengthOfMessageToSend){
 
 void rxCallback(uint8_t *buffer, uint8_t len)
 {
-  /*if((char)buffer[0] == 'a'){
-    playSound();
+  if((char)buffer[0] == 'a'){
+    //Serial.println("red and green");
+    rgb(0, 255, 0, 0);
+    rgb(1, 0, 255, 0);
+    leds.show();
+  }
+  
+  if((char)buffer[0] == 'b'){
+    //Serial.println("purple and blue");
+    rgb(0, 255, 0, 250);
+    rgb(1, 0, 0, 255);
+    leds.show();
   }
 
-  if((char)buffer[0] == 'b'){
+  if((char)buffer[0] == 'c'){
     sendMessage("hello", 5);
-  }*/
+  }
 }
 
 /*==============================*/
@@ -71,6 +101,9 @@ void setup(void)
 { 
   Serial.begin(9600);
   while(!Serial);
+  
+  leds.begin();
+  leds.show();
   
   /*pinMode(SPEAKER_PIN, OUTPUT);
 
@@ -96,6 +129,9 @@ void loop()
   time.loopStart();
 
   bluetooth.pollACI();
+  
+  rgb(15, 15, 0);
+  leds.show();
 
   time.loopEnd();
 }
